421507f36f6a9cf00b92c24df9a1e08e6af48528
c0nnex <support@fsgs.com>
2016-09-21 21:19:02 +0200

    Add Contract for remote controls

---
+++ SPAD.Interfaces/ServiceContract/IRemoteService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPAD.neXt.Interfaces.ServiceContract
+{
+
+    public sealed class RemoteServiceResponse
+    {
+        public bool HasError { get; set; }
+        public string Error { get; set; }
+        public double Value { get; set; }
+    }
+
+    [ServiceContract]
+    [ServiceKnownType(typeof(RemoteServiceResponse))]
+    public interface IRemoteService
+    {
+        [OperationContract]
+        RemoteServiceResponse GetValue(string variableName);
+
+        [OperationContract]
+        RemoteServiceResponse SetValue(string variableName, double newValue);
+
+        [OperationContract]
+        RemoteServiceResponse EmulateEvent(string eventTarget, string eventName, string eventParameter);
+    }
+}
